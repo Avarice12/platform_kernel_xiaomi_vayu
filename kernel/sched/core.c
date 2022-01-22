83a839a7b823c3818eb2fc2b84c02d164bbf3025
Rafiester <rafi.haqqi@gmail.com>
2022-01-22 16:47:24 +0700

    Revert "sched/core: Export symbols needed by modules"
    This reverts commit ecc8678dd604fba5d0893c352dc06d5a85f7b15a.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1006,7 +1006,6 @@ unsigned long uclamp_eff_value(struct task_struct *p, enum uclamp_id clamp_id)
 
 	return (unsigned long)uc_eff.value;
 }
-EXPORT_SYMBOL_GPL(uclamp_eff_value);
 
 /*
  * When a task is enqueued on a rq, the clamp bucket currently defined by the
@@ -5042,7 +5041,6 @@ int idle_cpu(int cpu)
 
 	return 1;
 }
-EXPORT_SYMBOL_GPL(idle_cpu);
 
 /**
  * idle_task - return the idle task for a given CPU.
