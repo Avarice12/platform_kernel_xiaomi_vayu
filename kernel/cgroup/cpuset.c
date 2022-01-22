7f8eca074a2c12899319c499627d3055dae8102b
Rafiester <rafi.haqqi@gmail.com>
2022-01-22 16:45:05 +0700

    Revert "uclamp_assist: Tune values"
    This reverts commit 442e65970084d7030e25f63a06fe93c39843035a.

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2077,13 +2077,13 @@ static void uclamp_set(struct kernfs_open_file *of,
 	const char *cs_name = cs->css.cgroup->kn->name;
 
 	static struct ucl_param tgts[] = {
-		{"top-app",		"20", "max",  0, 1},
-		{"audio-app",		"10", "40",   0, 0},
-		{"foreground",		"10", "100",  0, 1},
-		{"restricted",		"50", "100",  0, 1},
-		{"camera-daemon",	"10", "50",   0, 0},
-		{"background",		"20", "max",  1, 1},
-		{"system-background",	"20", "100",  0, 1},
+		{"top-app",    	     	"10", "max", 1, 1},
+		{"foreground", 	     	"0",  "50",  0, 0},
+		{"background", 	     	"20", "max", 0, 0},
+		{"system-background", 	"0",  "40",  0, 0},
+		{"camera-daemon",	"0", "50",   0, 0},
+		{"background",		"20", "100", 0, 0},
+		{"system-background",	"0",  "40",  0, 0},
 	};
 
 	for (i = 0; i < ARRAY_SIZE(tgts); i++) {
