76d64b81113a79eb15b7d3da94d2b9aacb15acbf
Rafiester <rafi.haqqi@gmail.com>
2022-05-24 18:25:24 +0700

    drivers: staging: Checkout qca-wifi-host-cmn to chaldea-kernel
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>

--- drivers/staging/qca-wifi-host-cmn/umac/dfs/core/src/misc/dfs_nol.c
+++ drivers/staging/qca-wifi-host-cmn/umac/dfs/core/src/misc/dfs_nol.c
@@ -355,7 +355,6 @@ static os_timer_func(dfs_remove_from_nol)
 #endif
 #endif
 
-#ifdef DFS_DEBUG
 void dfs_print_nol(struct wlan_dfs *dfs)
 {
 	struct dfs_nolelem *nol;
@@ -383,7 +382,6 @@ void dfs_print_nol(struct wlan_dfs *dfs)
 		nol = nol->nol_next;
 	}
 }
-#endif
 
 void dfs_print_nolhistory(struct wlan_dfs *dfs)
 {
