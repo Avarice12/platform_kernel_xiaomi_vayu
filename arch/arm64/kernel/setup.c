d678462babc03f50e18f566583cc09b445adb69e
rafiester <rafi.haqqi@gmail.com>
2022-08-23 14:48:42 -0400

    arm64: fix missmerge
    Signed-off-by: rafiester <rafi.haqqi@gmail.com>

--- arch/arm64/kernel/setup.c
+++ arch/arm64/kernel/setup.c
@@ -303,16 +303,11 @@ static void __init setup_machine_fdt(phys_addr_t dt_phys)
 			cpu_relax();
 	}
 
-<<<<<<< HEAD
-	machine_name = arch_read_machine_name();
-	if (!machine_name)
-=======
 	/* Early fixups are done, map the FDT as read-only now */
 	fixmap_remap_fdt(dt_phys, &size, PAGE_KERNEL_RO);
 
-	name = of_flat_dt_get_machine_name();
-	if (!name)
->>>>>>> c9349154782e2f5efa01adfa6c5e9411359a3845
+	machine_name = arch_read_machine_name();
+	if (!machine_name)
 		return;
 
 	pr_info("Machine: %s\n", machine_name);
