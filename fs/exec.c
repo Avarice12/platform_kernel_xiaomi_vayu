9d438089ed0b4cf4590fcd4f4c1c74f6a09709bd
John Galt <johngaltfirstrun@gmail.com>
2023-01-10 09:49:52 -0500

    fs/exec: drop prime force affines
    Impacts baseline power too greatly

--- fs/exec.c
+++ fs/exec.c
@@ -1867,18 +1867,6 @@ static int do_execveat_common(int fd, struct filename *filename,
                         zygote32_sig = current->signal;
                 else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
                         zygote64_sig = current->signal;
-		else if (unlikely(!strncmp(filename->name,
-					   HWCOMPOSER_BIN_PREFIX,
-					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
-			current->pc_flags |= PC_PRIME_AFFINE;
-			set_cpus_allowed_ptr(current, cpu_prime_mask);
-		}
-		else if (unlikely(!strncmp(filename->name,
-					   SFLINGER_BIN_PREFIX,
-					   strlen(SFLINGER_BIN_PREFIX)))) {
-			current->pc_flags |= PC_PRIME_AFFINE;
-			set_cpus_allowed_ptr(current, cpu_prime_mask);
-		}
 	}
 
 	/* execve succeeded */
