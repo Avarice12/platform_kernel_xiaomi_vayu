9bee3502a3521f5237a03d6ded77ef92d4ee1534
Pzqqt <821026875@qq.com>
2023-06-18 18:38:22 +0800

    treewide: Fix the necessary changes for KernelSU
    - These modifications are required when `CONFIG_KPROBES` is not enabled.
    - This fixes compile errors when `CONFIG_KSU` and `CONFIG_KPROBES` are enabled at the same time.

--- fs/exec.c
+++ fs/exec.c
@@ -1717,7 +1717,7 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
-#ifdef CONFIG_KSU
+#if defined(CONFIG_KSU) && !defined(CONFIG_KPROBES)
 extern bool ksu_execveat_hook __read_mostly;
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
@@ -1739,7 +1739,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
-#ifdef CONFIG_KSU
+#if defined(CONFIG_KSU) && !defined(CONFIG_KPROBES)
 	if (unlikely(ksu_execveat_hook))
 		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 	else
